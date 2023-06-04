49efe96936a7ee7bfc753e565c4b5e438c9fc364
jpgb-hub <jpgbonnet@gmail.com>
2023-06-03 22:53:32 -0400

    guarda

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+  var pronoun = ['the', 'our'];
+  var adj = ['great', 'big'];
+  var noun = ['jogger', 'racoon'];
+  var domain = ['.com'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + domain[l])
+        }
+      }
+    }
+  }
 };
